6c53b20f3226a5191906e9077f4eda6959e7f9e8
junqiuwang <junqiuwang@tencent.com>
2021-07-25 18:57:08 +0800

    0725

--- publis_gitbook.sh
+++ publis_gitbook.sh
@@ -1,5 +1,24 @@
 !#/bin/bash
 
 # gitbook
-gitbook init 
-gitbook build
+gitbook init && gitbook build
+
+
+git subtree add --prefix=_book
+
+
+
+git subtree add   --prefix=_book "add gitbook"
+
+git subtree pull --prefix=_book origin gh-pages
+git subtree push --prefix=_book origin gh-pages
+
+
+
+git subtree push --prefix=_book origin gh-pages
+
+
+git symbolic-ref HEAD refs/heads/gh-pages
+git add .
+git commit -m "gitbook rebuild"
+git push --force origin gh-pages
